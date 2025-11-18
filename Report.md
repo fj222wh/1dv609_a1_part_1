cc6cacd517abb6635e67d36271dbebab940a9f56
Filippa Johansson <fj222wh@student.lnu.se>
2025-11-18 18:11:44 +0100

    Add test for short password

---
+++ Report.md
@@ -0,0 +1,12 @@
+# Test Suite Coverage
+*Create a table like this one for all the bugs and Tests, showing which test fails or succeeds for each test and the coverage. Use the table to remove tests that are redundant. You may shorten the bug-names to get a slimmer table.*
+
+| Version | Correct | BugDoesNotHash | NoTrim |  ... | MyCustomBug |
+| --- | ---| --- | --- | --- | --- |
+| Test name 1 | ✅ | ❌ | ✅ | ✅ | ✅ |
+| Test name 2 | ✅ | ✅ | ❌ | ✅ | ✅ |
+| Test name 3 | ✅ | ✅ | ❌ | ❌ | ✅ |
+| Coverage | 100% | 100% | 100% | 100% | 100% |
+
+
+
--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -1,7 +1,7 @@
 
 // Select one of the Password versions to test
 
-import { Password } from '../src/BugDoesNotHash'
+// import { Password } from '../src/BugDoesNotHash'
 // import { Password } from '../src/BugDoesNotTrim'
 // import { Password } from '../src/BugisPasswordAlwaysSame'
 // import { Password } from '../src/BugMissingNumberCheck'
@@ -11,14 +11,15 @@ import { Password } from '../src/BugDoesNotHash'
 // import { Password } from '../src/BugVeryShort'
 // import { Password } from '../src/BugWrongHashingAlgorithm'
 // import { Password } from '../src/BugWrongMessage'
-// import { Password } from '../src/Correct'
+import { Password } from '../src/Correct'
 
 describe('Password class, test suite', () => {
     //put constants here to increase readability
     const emptyPassword = '';
 
-    test('replace this test with one of your own and add more', () => {
-        expect(true).toBe(true);
+    test('Constructor Should Throw Exception For Short Passwords', () => {
+    
+    expect(() => new Password('fkfo')).toThrow('Too short password')
     });
 
     //Add your tests here
