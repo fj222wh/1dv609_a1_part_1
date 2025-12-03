b236f45562b865b341b6ec31f380e3c7ef4cc380
Filippa Johansson <fj222wh@student.lnu.se>
2025-12-03 17:23:38 +0100

    Add test for SNN-helper to check the format

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -6,6 +6,8 @@ describe('SSNHelper Tests', () => {
     const validInput = '991218-4382'
     const oneCharLessThanMinimum = '991218-438'
     const oneCharMoreThanMinimum = '991218-43889'
+    const invalidFormat = '9912-173948'
+
     const ssnHelper = new SSNHelper()
     //put constants here to increase readability
 
@@ -21,6 +23,15 @@ describe('SSNHelper Tests', () => {
     test('Should Return False If Length Is Too Long', () => {
         expect(ssnHelper.isCorrectLength(oneCharMoreThanMinimum)).toBe(false)
     });
+
+    test('Should return True If Format Is Valid', () => {
+        expect(ssnHelper.isCorrectFormat(validInput)).toBe(true)
+    })
+
+    test('Should Return False If Format Is Invalid', () => {
+        expect(ssnHelper.isCorrectFormat(invalidFormat)).toBe(false)
+    })
+
 
   
 
