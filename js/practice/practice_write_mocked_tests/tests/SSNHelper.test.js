f121b788dbb69538594103a87312d0ffe927c57b
Filippa Johansson <fj222wh@student.lnu.se>
2025-12-03 17:18:20 +0100

    Add test for SNN-helper to check if the length is correct

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -2,10 +2,16 @@ import { SSNHelper } from '../src/correct/SSNHelper';
 
 
 
-describe('SSNHelpe Tests', () => {
+describe('SSNHelper Tests', () => {
+    const validInput = '991218-4382'
+    const oneCharLessThanMinimum = '991218-438'
+    const oneCharMoreThanMinimum = '991218-43889'
+    const ssnHelper = new SSNHelper()
+    //put constants here to increase readability
 
-    test('replace this test with one of your own', () => {
-        expect(true).toBe(true);
+
+    test('Should Not Throw If Length Is Correct', () => {
+        expect(ssnHelper.isCorrectLength(correctLength)).toBe(true)
     });
 
 });
--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -4,11 +4,9 @@ import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurit
 
 //NOTE THESE TESTS SHOULD NOT BE DEPENDENT ON SSNHelper BUT USE MOCKING
 describe('SwedishSocialSecurityNumber Tests', () => {
-    //put constants here to increase readability
 
-    test('replace this test with one of your own', () => {
-        expect(true).toBe(true);
-    });
+    test('Test', () => {
+        expect(true)
+    })
 
-    //Add your tests here
 });
