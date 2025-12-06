ca59d2ea8af5d757f60ecdb3b70c82cdab3c2646
Filippa Johansson <fj222wh@student.lnu.se>
2025-12-06 20:06:47 +0100

    Add mock test for short password

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,12 +1,7 @@
 import { expect, jest, test } from '@jest/globals'
-import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber';
-// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySSNHelperAllowDayUpTo30';
-// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySSNHelperAllowMonth0';
-// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySSNHelperIncorrectFormat';
-// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySSNHelperMessyLuhn';
-// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySSNHelperWrongLength';
+// import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber.js';
 // import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck';
-// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim';
+import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim';
 // import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberNoLuhn';
 // import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongYear';
 
@@ -14,10 +9,38 @@ import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurit
 //NOTE THESE TESTS SHOULD NOT BE DEPENDENT ON SSNHelper BUT USE MOCKING
 describe('SwedishSocialSecurityNumber Tests', () => {
 
-    const validInput = '900515-1239'
-    const tooShort = '991217543'
+    const snn_validInput = '900515-1239'
+    const snn_invalid = '991203-1453'
+    const snn_too_short = '900515-123'
+    const snn_oneCharMoreThanMinimum = '900515-12393'
+    const snn_invalidFormat = '90051512-39'
 
-    test('Test', () => {
+    const validMonthMin = '1'
+    const validMonthMax = '12'
+    const invalidMonthOneBelowMin = '0'
+    const invalidMonthOneAboveMax = '13'
+    const invalidMonth = 'hej'
+
+    const validDay = '15'
+    const validDayMin = '1'
+    const validDayMax = '31'
+    const invalidDayOneBelowMin = '0'
+    const invalidDayOneAboveMax = '32'
+    const invalidDayNotANumber = 'hej'
+
+    test('Should be able to create a SNN with valid input', () => {
+        const mockHelper = {
+            isCorrectLength: jest.fn().mockReturnValue(true),
+            isCorrectFormat: jest.fn().mockReturnValue(true),
+            isValidMonth: jest.fn().mockReturnValue(true),
+            isValidDay: jest.fn().mockReturnValue(true),
+            luhnisCorrect: jest.fn().mockReturnValue(true)
+        }
+        expect(() => new SwedishSocialSecurityNumber(snn_validInput, mockHelper)).not.toThrow();
+
+    })
+
+    test('Constructor Should Throw Error If SSN is too short', () => {
         const mockHelper = {
             isCorrectLength: jest.fn().mockReturnValue(false),
             isCorrectFormat: jest.fn().mockReturnValue(false),
@@ -26,8 +49,9 @@ describe('SwedishSocialSecurityNumber Tests', () => {
             luhnisCorrect: jest.fn().mockReturnValue(true)
         }
 
-        expect(() => new SwedishSocialSecurityNumber(tooShort, mockHelper)).toThrow("To short, must be 11 characters");
-
+    
+        expect(() => new SwedishSocialSecurityNumber(snn_too_short, mockHelper)).toThrow("To short, must be 11 characters");
+  
     })
 
 });
