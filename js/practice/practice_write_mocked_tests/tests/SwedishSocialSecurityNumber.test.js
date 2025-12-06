35c0e82df2521e53959dd4c3e5bbed252703d83d
Filippa Johansson <fj222wh@student.lnu.se>
2025-12-06 22:03:11 +0100

    Add tests for error messages for invalid day and month in SwedishSocialSecurityNumber

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -11,25 +11,13 @@ describe('SwedishSocialSecurityNumber Tests', () => {
 
     const snn_validInput = '900515-1239'
     const snn_validInput_with_spaces = '  900515-1239'
-    const snn_invalid = '991203-1453'
     const snn_too_short = '900515-123'
-    const snn_oneCharMoreThanMinimum = '900515-12393'
     const snn_invalidFormat = '90051512-39'
+    const snn_invalidMonth = '901316-1239'
+    const snn_invalidDay = '901234-1239'
+    const snn_invalidLuhnis = '991217-3686'
 
-    // DO WE NEED MORE TESTS FOR EACH POSSIBLE DATA OR IS IT REDUNDANT?
 
-    const validMonthMin = '1'
-    const validMonthMax = '12'
-    const invalidMonthOneBelowMin = '0'
-    const invalidMonthOneAboveMax = '13'
-    const invalidMonth = '13'
-
-    const validDay = '15'
-    const validDayMin = '1'
-    const validDayMax = '31'
-    const invalidDayOneBelowMin = '0'
-    const invalidDayOneAboveMax = '32'
-    const invalidDayNotANumber = 'hej'
 
     test('Should be able to create a SNN with valid input', () => {
         const mockHelper = {
@@ -54,10 +42,10 @@ describe('SwedishSocialSecurityNumber Tests', () => {
 
     
         expect(() => new SwedishSocialSecurityNumber(snn_too_short, mockHelper)).toThrow("To short, must be 11 characters");
-  
+
     })
 
-    test('Constructor Should Throw Error If SSN is too short', () => {
+    test('Constructor Should Throw Error If SSN Has Invalid Format', () => {
         const mockHelper = {
             isCorrectLength: jest.fn().mockReturnValue(true),
             isCorrectFormat: jest.fn().mockReturnValue(false),
@@ -68,6 +56,31 @@ describe('SwedishSocialSecurityNumber Tests', () => {
 
         expect(() => new SwedishSocialSecurityNumber(snn_invalidFormat, mockHelper)).toThrow("Incorrect format, must be: YYMMDD-XXXX");
   
+    })
+
+    test('Constructor Should Throw Error If Invalid Month', () => {
+        const mockHelper = {
+            isCorrectLength: jest.fn().mockReturnValue(true),
+            isCorrectFormat: jest.fn().mockReturnValue(true),
+            isValidMonth: jest.fn().mockReturnValue(false),
+            isValidDay: jest.fn().mockReturnValue(true),
+            luhnisCorrect: jest.fn().mockReturnValue(true)
+        }
+
+        expect(() => new SwedishSocialSecurityNumber(snn_invalidMonth, mockHelper)).toThrow('Invalid month in SSN')
+
+    })
+
+    test('Constructor Should Throw Error If Invalid Day', () => {
+        const mockHelper = {
+            isCorrectLength: jest.fn().mockReturnValue(true),
+            isCorrectFormat: jest.fn().mockReturnValue(true),
+            isValidMonth: jest.fn().mockReturnValue(true),
+            isValidDay: jest.fn().mockReturnValue(false),
+            luhnisCorrect: jest.fn().mockReturnValue(true)
+        }
+
+        expect(() => new SwedishSocialSecurityNumber(snn_invalidDay, mockHelper)).toThrow('Invalid day in SSN')
     })
 
     test('Should Trim White Spaces', () => {
@@ -123,6 +136,19 @@ describe('SwedishSocialSecurityNumber Tests', () => {
 
         const sut = new SwedishSocialSecurityNumber(snn_validInput, mockHelper)
         expect(sut.getDay()).toBe('15')
+    })
+
+    test('Should Return The Corresponding Day', () => {
+        const mockHelper = {
+            isCorrectLength: jest.fn().mockReturnValue(true),
+            isCorrectFormat: jest.fn().mockReturnValue(true),
+            isValidMonth: jest.fn().mockReturnValue(true),
+            isValidDay: jest.fn().mockReturnValue(true),
+            luhnisCorrect: jest.fn().mockReturnValue(true)
+        }
+
+        const sut = new SwedishSocialSecurityNumber(snn_validInput, mockHelper)
+        expect(sut.getSerialNumber()).toBe('1239')
     })
 
 });
